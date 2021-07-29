197ca2e50556e572335da7b11ec97487a899ba9d
Jan <hiasguy@hotmail.com>
2021-07-29 20:07:29 +0200

    added firebase code

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBD4e45ieMgpx_4fcqs5Rer7sZtSD0-Jqg",
+    authDomain: "da-kanbanboard.firebaseapp.com",
+    projectId: "da-kanbanboard",
+    storageBucket: "da-kanbanboard.appspot.com",
+    messagingSenderId: "446763488074",
+    appId: "1:446763488074:web:31b3c9970bf23225e5e7b3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
