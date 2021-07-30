238ca07eda7f3bdd03fe6af39e59e26077bf8784
Jan <hiasguy@hotmail.com>
2021-07-30 15:41:06 +0200

    added addTask function at addTask.js and added needed firestore code in different files

--- js/addTask.js
+++ js/addTask.js
@@ -1,28 +1,42 @@
 function addTaskCreate() {
-
-
-    let title = document.getElementById("input-title").value;
+    let title = document.getElementById('addTaskTitle').value;
+    let date = document.getElementById('addTaskDueDate').value;
+    let category = document.getElementById('addTaskCategory').value;
+    let urgency = document.getElementById('addTaskUrgency').value;
+    let description = document.getElementById('addTaskDescription').value;
     title = title.trim();
+    description = description.trim();  //trim löscht die leerzeichen am anfang und am ende eines textes
+
+    var db = firebase.firestore();
+
     if (title.length == 0) {
-        console.error("Title is Empty");
+        alert('Please enter a title!');
     } else {
-        
-        // Add a new document with a generated id.
-        db.collection("tasks").add({
-            //TODO Gibt die Werte aus dem Formular hier rein.
-            title: title,
-            description: "Japan",
-            urgency: "",
-            datum: "",
-            //:::::
-        })
-            .then((docRef) => {
-                console.log("Document written with ID: ", docRef.id);
+        if (description.length == 0) {
+            alert('Please enter a description!');
+        } else {
+            if (date.length == 0) {
+                alert('Please enter a due date!');
+            } else {
+                db.collection("tasks").add({
+                    title: title,
+                    duedate: date,
+                    category: category,
+                    urgency: urgency,
+                    description: description,
+                    status: 'todo',
 
-            })
-            .catch((error) => {
-                console.error("Error adding document: ", error);
+                })
+                    .then((docRef) => {
+                        console.log("Document written with ID: ", docRef.id);
+                        window.location.assign("board.html");
 
-            });
+                    })
+                    .catch((error) => {
+                        console.error("Error adding document: ", error);
+
+                    });
+            }
+        }
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
 function init() {
     includeHTML();
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        initNavBar(user);
+        if (user) {
+            console.log(user);
+            // User is signed in.
+            //window.location.assign("addTask.html");
+        } else {
+            // No user is signed in.
+            // The start method will wait until the DOM is loaded.
+            ui.start('#firebaseui-auth-container', uiConfig);
+        }
+    });
+}
+
+function setProfile(user) {
+    // Add a new document with a generated id.
+    firebase.firestore().collection("users").doc(user.uid).set({
+        displayName: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+        emailVerified: user.emailVerified,
+        uid: user.uid,
+        isAnonymous: user.isAnonymous
+    });
 }
