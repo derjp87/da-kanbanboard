b4ef284e11156b67f96b7ed0217e9634ad52c061
Jan <hiasguy@hotmail.com>
2021-08-09 20:20:52 +0200

    added drag&drop code to board

--- js/board.js
+++ js/board.js
@@ -9,8 +9,8 @@ async function loadTasks() {
     let tasks = firebase.firestore().collection('tasks');
     let response = await tasks.get();
     response.forEach((i) => {
-        console.log(i.data());
-        allTasks.push(i.data());
+         console.log(i.data());
+         allTasks.push(i.data());
 
     });
     document.getElementById('boardToDo').innerHTML = '';
@@ -23,22 +23,34 @@ async function loadTasks() {
 
         if (tasksStatus == 'todo') {
             document.getElementById('boardToDo').innerHTML += `
-            <div class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
+            <div draggable="true" ondragstart="startDragging(${allTasks['id']})" class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
         } else {
             if (tasksStatus == 'inprogress') {
                 document.getElementById('boardInProgress').innerHTML += `
-                <div class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
+                <div draggable="true" ondragstart="startDragging(${allTasks['id']})" class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
             } else {
                 if (tasksStatus == 'testing') {
                     document.getElementById('boardTesting').innerHTML += `
-                    <div class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
+                    <div draggable="true" ondragstart="startDragging(${allTasks['id']})" class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
                 } else {
                     document.getElementById('boardDone').innerHTML += `
-                    <div class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
+                    <div draggable="true" ondragstart="startDragging(${allTasks['id']})" class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
                 }
             }
         }
     }
+}
+
+function startDragging(id) {
+    currentDraggedElement = id;
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function moveTo(status) {
+    allTasks[currentDraggedElement]['status'] = status;
 }
 
 function tasksDetails(i) {
