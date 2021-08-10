9e53b29621af2a410450edb6b7caf3a638291cd3
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-08-10 16:59:47 +0200

    backlog style added

---
+++ js/backlog.js
@@ -0,0 +1,20 @@
+let allBacklogs = [];
+
+function renderBacklog() {
+
+let backlogContainer = document.getElementById('mainContent');
+backlogContainer.innerHTML = ``;
+
+for (let i = 0; i < allBacklogs.length; i++) {
+    let backlog = allBacklogs[i];
+    backlogContainer.innerHTML = ``;
+
+    
+}
+
+}
+
+function addBacklogEntry() {
+
+
+}
