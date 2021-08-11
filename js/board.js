cf765d053468cd249276921882520eedb2d0a8ee
Jan <hiasguy@hotmail.com>
2021-08-11 17:14:04 +0200

    cleaned code

--- js/board.js
+++ js/board.js
@@ -51,37 +51,10 @@ function moveTo(status) {
     let taskToMove = allTasks.find ( task => task.id == currentDraggedElement );
     taskToMove.status = status;
     firebase.firestore().collection('tasks').doc(currentDraggedElement).set(taskToMove);
-    reloadTasks();
+    loadTasks();
 }
 
-function reloadTasks() {
-    document.getElementById('boardToDo').innerHTML = '';
-    document.getElementById('boardInProgress').innerHTML = '';
-    document.getElementById('boardTesting').innerHTML = '';
-    document.getElementById('boardDone').innerHTML = '';
 
-    for (let i = 0; i < allTasks.length; i++) {
-        let tasksStatus = allTasks[i]['status'];
-
-        if (tasksStatus == 'todo') {
-            document.getElementById('boardToDo').innerHTML += `
-            <div draggable="true" ondragstart="startDragging(${allTasks[i]['id']})" class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
-        } else {
-            if (tasksStatus == 'inprogress') {
-                document.getElementById('boardInProgress').innerHTML += `
-                <div draggable="true" ondragstart="startDragging(${allTasks[i]['id']})" class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
-            } else {
-                if (tasksStatus == 'testing') {
-                    document.getElementById('boardTesting').innerHTML += `
-                    <div draggable="true" ondragstart="startDragging(${allTasks[i]['id']})" class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
-                } else {
-                    document.getElementById('boardDone').innerHTML += `
-                    <div draggable="true" ondragstart="startDragging(${allTasks[i]['id']})" class="board-task" onclick="tasksDetails(${i})">${allTasks[i]['title']}</div>`;
-                }
-            }
-        }
-    }
-}
 
 function tasksDetails(i) {
 
