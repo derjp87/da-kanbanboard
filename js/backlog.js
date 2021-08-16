641eaa9d78604feec86da55b72e5540fa26c8787
Jan <hiasguy@hotmail.com>
2021-08-16 19:54:42 +0200

    added code to backlog.js, backlog.css and changed code on addTask.html

--- js/backlog.js
+++ js/backlog.js
@@ -1,50 +1,59 @@
 function init() {
 
   firebase.auth().onAuthStateChanged(async function (user) {
-      if (user) {
-          // User is signed in.
-          includeHTML();
-          //loadTasks();
-          //showUserList();
-          renderBacklog();
-          //initNavBar(user);
-          //setDateMinToday();
-          //setTasks(user.uid);
-          //setUsers();
-      } else {
-          // No user is signed in.
-          window.location.assign("index.html");
-      }
+    if (user) {
+      // User is signed in.
+      includeHTML();
+      //loadTasks();
+      //showUserList();
+      renderBacklog();
+      //initNavBar(user);
+      //setDateMinToday();
+      //setTasks(user.uid);
+      //setUsers();
+    } else {
+      // No user is signed in.
+      window.location.assign("index.html");
+    }
   });
 }
 
 let allBackLogs = [];
 
-function renderBacklog() {
+async function renderBacklog() {
+  allBackLogs = [];
+  let backLogs = firebase.firestore().collection('tasks'); // hier wird definiert, auf was er bei backLogs zugreifen soll. Datenbankpfad zu den Tasks!
+  let response = await backLogs.get(); // er soll warten bis er die "backLogs" heruntergeladen hat, deswegen await und deswegen ist dies eine "async function"
+  response.forEach((i) => { // er soll jeden einzelnen Eintrag in der collection "tasks" herunterladen
+    console.log(i.data(), i.id);
+    let backLogs = i.data();
+    backLogs.id = i.id;
+    allBackLogs.push(backLogs);
+  });
+
   let backlogContainer = document.getElementById('mainContent');
 
-  if(allBackLogs.length > 0) {
-// backlogContainer.innerHTML = ``;
- // for (let i = 0; i < allBacklogs.length; i++) {
-   // let backlog = allBacklogs[i];
-    backlogContainer.innerHTML += `
-    <div class="todo-container">
-    <div class="assigned-to">
-      <div id="backlog-color" class="category-color"></div>
-      <img id="backlog-img" class="personal-img" src=""/>
-      <div class="personal-data">
-        <span id="backlog-name" class="name"></span>
-        <a id="backlog-email" href="#"></a>
-      </div>
-    </div>
-    <div id="backlog-category" class="category"></div>
-    <div id="backlog-details" class="details">
-    </div>
-  </div>
-    `;    
-} else {
-  backlogContainer.innerHTML = `<div class="todo-container no-entries">"Keine Einträge vorhanden..."</div>`;
+  if (allBackLogs.length > 0) {
+    backlogContainer.innerHTML = ``;
+    for (let i = 0; i < allBackLogs.length; i++) {
 
-}
+      backlogContainer.innerHTML += `
+        <div class="todo-container">
+          <div class="assigned-to">
+            <div id="backlog-color" class="category-color" style="background-color: ${allBackLogs[i]['taskcolor']};"></div>
+              <div class="personal-data">
+                <span id="backlog-name" class="name">${allBackLogs[i]['assignedusers']}</span>
+                  <a id="backlog-email" href="#"></a>
+              </div>
+          </div>
+          <div id="backlog-category" class="category">${allBackLogs[i]['category']}</div>
+          <div id="backlog-details" class="details"></div>
+        </div>
+        `;
+    }
+  } else {
+    backlogContainer.innerHTML = `<div class="todo-container no-entries">"Keine Einträge vorhanden..."</div>`;
+
+  }
 
 }
