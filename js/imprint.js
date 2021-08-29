22e6ea91fe3572b27b4c94bbf92a062a9831536c
Jan <hiasguy@hotmail.com>
2021-08-29 17:56:39 +0200

    added content to index

---
+++ js/imprint.js
@@ -0,0 +1,20 @@
+function init() {
+
+    firebase.auth().onAuthStateChanged(async function (user) {
+        if (user) {
+            // User is signed in.
+            includeHTML();
+            //loadTasks();
+            //showUserList();
+            //initNavBar(user);
+            //setDateMinToday();
+            //setTasks(user.uid);
+            //setUsers();
+        } else {
+            // No user is signed in.
+            includeHTML();
+            setTimeout(document.getElementById('logoutButton').classList.add('d-none'), 3000);
+            
+        }
+    });
+  }
