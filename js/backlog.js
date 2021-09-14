b1be3b3831559ad1f6bb2d5233879845fa9d707f
Jan <hiasguy@hotmail.com>
2021-09-14 20:59:18 +0200

    smaller backlog.js functions

--- js/backlog.js
+++ js/backlog.js
@@ -1,11 +1,8 @@
 function init() {
-
   firebase.auth().onAuthStateChanged(async function (user) {
     if (user) { // User is signed in.
-     includeHTML();
-
+      includeHTML();
       renderBacklog();
-    
     } else { // No user is signed in.
       window.location.assign("index.html");
     }
@@ -16,40 +13,43 @@ let allBackLogs = [];
 
 async function renderBacklog() {
   allBackLogs = [];
-  let backLogs = firebase.firestore().collection('tasks'); // hier wird definiert, auf was er bei backLogs zugreifen soll. Datenbankpfad zu den Tasks!
-  let response = await backLogs.get(); // er soll warten bis er die "backLogs" heruntergeladen hat, deswegen await und deswegen ist dies eine "async function"
-  response.forEach((i) => { // er soll jeden einzelnen Eintrag in der collection "tasks" herunterladen
+  let backLogs = firebase.firestore().collection('tasks');
+  let response = await backLogs.get();
+  response.forEach((i) => {
     console.log(i.data(), i.id);
     let backLogs = i.data();
     backLogs.id = i.id;
     allBackLogs.push(backLogs);
   });
-  
-  let backlogContainer = document.getElementById('mainContent');
+  showBacklog();
+}
 
+function showBacklog() {
   if (allBackLogs.length > 0) {
-    backlogContainer.innerHTML = ``;
+    document.getElementById('mainContent').innerHTML = ``;
     for (let i = 0; i < allBackLogs.length; i++) {
-
-      backlogContainer.innerHTML += `
-        <div class="todo-container" onclick="openBoard()">
-          <div class="assigned-to">
-            <div id="backlog-color" class="category-color" style="background-color: ${allBackLogs[i]['taskcolor']};"></div>
-              <div class="personal-data">
-                <span id="backlog-name" class="name">${allBackLogs[i]['assignedusers']}</span>
-                  <a id="backlog-email" href="#"></a>
-              </div>
-          </div>
-          <div id="backlog-category" class="category">${allBackLogs[i]['category']}</div>
-          <div id="backlog-details" class="details">${allBackLogs[i]['title']}</div>
-        </div>
-        `;
-    }  
+      loadBacklogs(i);
   }
-   else {
+  } else {
     backlogContainer.innerHTML = `<div class="todo-container no-entries">"Keine Einträge vorhanden..."</div>`;
-   }
   }
+}
+
+function loadBacklogs(i) {
+  document.getElementById('mainContent').innerHTML += `
+  <div class="todo-container" onclick="openBoard()">
+    <div class="assigned-to">
+      <div id="backlog-color" class="category-color" style="background-color: ${allBackLogs[i]['taskcolor']};"></div>
+        <div class="personal-data">
+          <span id="backlog-name" class="name">${allBackLogs[i]['assignedusers']}</span>
+            <a id="backlog-email" href="#"></a>
+        </div>
+    </div>
+    <div id="backlog-category" class="category">${allBackLogs[i]['category']}</div>
+    <div id="backlog-details" class="details">${allBackLogs[i]['title']}</div>
+  </div>
+  `;
+}
 
 function openBoard() {
   window.location.href = 'board.html';
