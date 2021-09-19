d328c393e4222cca3bdfa60cc734318dcf698c31
Mads Kristian Hansen <mads_hansen@hotmail.de>
2021-09-19 14:12:46 +0200

    addTask.js comments adapted

--- js/addTask.js
+++ js/addTask.js
@@ -1,11 +1,16 @@
+/**
+ * Checks if a user is signed in
+ * Initializes site if true
+ * Assigns back to Index if not
+ */
+
 function init() {
     firebase.auth().onAuthStateChanged(async function (user) {
         if (user) {
-            // User is signed in.
             includeHTML();
             showUserList();
         } else {
-            window.location.assign("index.html"); //if no user is signed in assign to index.
+            window.location.assign("index.html"); 
         }
     });
 }
@@ -13,53 +18,76 @@ function init() {
 let userList = [];
 let assignedUsers = [];
 
+/**
+ * Displays selectable UserList:
+ * Loads user data from firestore server
+ * Pushes user data into userList array + log data into console.
+ * Displays selectable userlist at addTask - except for anonymous user "null".
+ */
+
 async function showUserList() {
     userList = [];
     let users = firebase.firestore().collection('users');
     let response = await users.get();
-    //load user data from firestore server.
     response.forEach((i) => {
         console.log(i.data());
         userList.push(i.data());
     });
-    //push user data into userList array + log data into console.
     document.getElementById('addTaskUserList').innerHTML = '';
     for (let i = 0; i < userList.length; i++) {
         if (JSON.stringify(userList[i]['displayName']) !== "null") {
             document.getElementById('addTaskUserList').innerHTML += `
-        <div class="addTask-addUser" onclick="addTaskAddUser(${i})">${userList[i]['displayName']}</div>`;
-        }
-    } //display selectable userlist at addTask - except for anonymous user "null".
+        <div class="addTask-addUser" onclick="addTaskAddUser(${i})">${userList[i]['displayName']}</div>`; }
+    } 
 }
+
+/**
+ * Adds and displays selected User to Assigned Users:
+ * If selected user from displayed userList is not included as assignedUsers, push data into assignedUsers array.
+ * Displays assigned users from assignedUsers array.
+ */
 
 function addTaskAddUser(i) {
     if (!assignedUsers.includes(userList[i]['displayName'])) {
         assignedUsers.push(userList[i]['displayName']);
-        //if selected user from displayed userList is not included as assignedUsers, push data into assignedUsers array.
         document.getElementById('addTaskAssignedTo').innerHTML = '';
 
         for (let i = 0; i < assignedUsers.length; i++) {
             document.getElementById('addTaskAssignedTo').innerHTML += `
             <div class="addTask-removeUser" onclick="addTaskRemoveUser(${i})">${assignedUsers[i]}</div>`;
-        } //display assigned users from assignedUsers array.
+        } 
     }
 }
 
+/**
+ * Removes selected assigned-User from assignedUsers-array.
+ * Clears and displays updated assigned users-list.
+ */
+
 function addTaskRemoveUser(i) {
     assignedUsers.splice(i, 1);
-    // remove selected assigned-user from assignedUsers array.
     document.getElementById('addTaskAssignedTo').innerHTML = '';
     for (let i = 0; i < assignedUsers.length; i++) {
         document.getElementById('addTaskAssignedTo').innerHTML += `
         <div class="addTask-removeUser" onclick="addTaskRemoveUser(${i})">${assignedUsers[i]}</div>`;
-    } //clear and display updated assigned users.
+    } 
 }
+
+/**
+ * Checks if at least one User has been assigned to Task in order to submit.
+ */
 
 function addTaskCreateCheck() {
     if (assignedUsers.length > 0) {
         document.getElementById('addTask-assignedTo-required').required = false;
     }
 }
+
+/**
+ * Submits input Task:
+ * Pushes input data.value of addTask-inputfields into collection 'tasks' at firestore server.
+ * Assigns user after submit to board.html.
+ */
 
 function addTaskCreate() {
     var db = firebase.firestore();
@@ -72,14 +100,17 @@ function addTaskCreate() {
         assignedusers: assignedUsers,
         status: 'todo',
         taskcolor: document.getElementById('addTaskColor').value,
-    }) //push input data.value of addTask-inputfields into collection 'tasks' at firestore server.
+    }) 
         .then((docRef) => {
             window.location.assign("board.html");
-        }); //assign user after submit to board.html.
+        }); 
 }
+
+/**
+ * Changes shown addTaskColor-field to selected option-color.
+ */
 
 function changeColorOfMenu() {
     let mymenu = document.getElementById('addTaskColor');
     mymenu.style = `background-color: ${mymenu.value};`;
-    // change shown addTaskColor-field to selected option-color.
 }
